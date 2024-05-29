eaa7d0398335de10f608f5833f89c476c10b0b8a
LaurenceT123 <laurencetremblay10@gmail.com>
2024-05-28 17:44:06 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Laurence Tremblay</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+       <>
+        <nav className="navbar">
+            <div className = "navbar-container">
+                <h1>test</h1>
+                <h1>test1</h1>
+                <h1>test2</h1>
+            </div>
+        </nav>
+       </>
+    )
+}
+
+export default Navbar
